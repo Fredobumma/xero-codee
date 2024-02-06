8dec5c42cc0658fe1843b7a2e73a591000deba14
Manfred Obumma <fredobumma@gmail.com>
2024-02-06 11:26:12 +0100

    --feat:: buttons structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,21 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<h1 className="font-bold text-[40px] leading-[48px] sm:font-semibold sm:text-[56px] lg:font-medium lg:text-[72px] lg:leading-[80px]">
-				TechSynergy: Innovate, Create, Elevate
-			</h1>
-			<p className="font-semibold text-[#F3BC4C] text-xs sm:text-sm lg:text-xl">
-				TECH SERVICES
-			</p>
-			<p className="font-medium text-sm sm:text-base lg:text-xl">
-				Unlocking Possibilities, One Code at a Time
-			</p>
-			<p className="font-medium text-2xl leading-[43px] lg:text-[32px] lg:leading-[60px]">
-				We have multidisciplinary teams to meet any{" "}
-				<span className="">challenge.</span>
-			</p>
-			<h2 className="font-medium text-2xl leading-[43px] md:text-[40px] md:leading-[60px]">
-				Empowering Your Digital Vision: Our Comprehensive Tech Services.
-			</h2>
-			<h2 className="font-medium text-[26px] leading-10 md:text-[40px] md:leading-[60px]">
-				Trusted by Leading Organizations
-			</h2>
-			<p className="font-medium text-xs leading-4 md:text-xl md:leading-[25px]">
-				Our 4,000+ team has expertise in almost everyprogramming language.
-			</p>
-			<h2 className="font-medium text-[26px] leading-10 md:text-[40px] md:leading-[54px]">
-				We've stopped counting. Over 500 brands count on us.
-			</h2>
-			<p className="font-medium text-xs leading-3 md:text-xl md:leading-[25px]">
-				Our 4,000+ team has expertise in almost everyprogramming language.
-			</p>
-			<h2 className="font-medium text-[26px] leading-[62px] md:text-[40px]">
-				Choose Us: Your Path to Innovation and <span className="">Success</span>
-			</h2>
-			<h2 className="font-medium text-[26px] leading-[62px] md:text-[40px]">
-				Let's Form Your Crew.
-			</h2>
-			<p className="font-medium text-lg leading-[23px] md:text-xl md:leading-7 lg:text-[32px] lg:leading-8">
-				Software Developers
-			</p>
-			<h2 className="font-semibold text-[32px] leading-[48px] md:text-[40px] md:leading-[58px] lg:text-5xl lg:leading-[68px]">
-				Ready to Transform Your Vision into Reality? Let's Get Started!
-			</h2>
-			<h4 className="leading-[28px]">Product</h4>
-			<p>Shift planner</p>
-			<p className="text-sm text-[#0A142F]">Terms</p>
+			<button className="bg-[#66B066] rounded-[42px] font-medium text-sm text-white py-[15px] px-9 md:text-base md:py-[18px] md:px-[34px] md:rounded-[50px]">
+				See projects
+			</button>
+			<button className="bg-[#3171DE] rounded-[42px] font-medium text-white py-[15px] px-9 md:py-[18px] md:px-[25px] md:rounded-[50px]">
+				Schedule a Call
+			</button>
+			<button className="bg-[#3171DE] rounded-[42px] font-medium text-white py-[15px] px-[25px] md:py-[18px] md:px-[34px] md:rounded-[50px]">
+				Our full repertorie
+			</button>
+			<button className="bg-[#3171DE] rounded-[42px] font-medium text-white py-[15px] px-[19px] md:py-[18px] md:px-[25px] md:rounded-[50px]">
+				Schedule a Call
+			</button>
+			<button className="bg-white rounded-[50px] font-semibold text-[#5E5DEF] py-[18px] px-[25px]">
+				Schedule a Call
+			</button>
 		</>
 	);
 }
